e16fd9704d952531dd30ffcc35a62f7b9304160c
Md. Mehedy Hasan Tanvir <52083575+Mehedy-Tanvir@users.noreply.github.com>
2023-02-03 20:32:41 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const bgImage = document.querySelector('#bg-image');
+window.addEventListener('scroll', () => {
+    updateImage();
+});
+function updateImage() {
+    bgImage.style.opacity = 1 - window.pageYOffset / 900;
+    bgImage.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
+}
